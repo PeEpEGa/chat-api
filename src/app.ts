ea2cc91f07da76ab81b6dbe22b884d2b4cf79819
Vitalii Yaltukhov <vitalii3797@gmail.com>
2024-12-06 18:18:16 +0200

    added cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import messageRouter from "./infrastructure/routes/message/messages.route";
 import userRouter from "./infrastructure/routes/user/user.route";
 import authRouter from "./infrastructure/routes/auth/auth.route";
 import chatRouter from "./infrastructure/routes/chat/chats-route";
+import cors from "cors";
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/messages", messageRouter);
